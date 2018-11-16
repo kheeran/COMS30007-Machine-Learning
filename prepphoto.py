8fff2ff2b60bc7b7ff6e69faca1a85948fa02dcb
Kheeran <kn16063@my.bristol.ac.uk>
2018-11-16 18:31:51 +0000

    removing image prep

--- prepphoto.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-
-img_temp = cv2.imread('Graphics/pic.png')
-_, img = cv2.threshold(img_temp,128, 255,cv2.THRESH_BINARY)
-cv2.imwrite('Graphics/stan_lee.png', img)
-
-
